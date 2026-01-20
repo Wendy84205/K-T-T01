ca561d3592d417839e752868a4f0237273a1e43f
wendy <wendy84205@gmail.com>
2026-01-20 20:08:11 +0700

    chore: update .gitignore and remove large files

--- backend/dist/database/entities/core/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Team = exports.User = void 0;
-var user_entity_1 = require("./user.entity");
-Object.defineProperty(exports, "User", { enumerable: true, get: function () { return user_entity_1.User; } });
-var team_entity_1 = require("../team-collaboration/team.entity");
-Object.defineProperty(exports, "Team", { enumerable: true, get: function () { return team_entity_1.Team; } });
-//# sourceMappingURL=index.js.map
